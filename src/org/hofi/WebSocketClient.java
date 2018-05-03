812887acfeefe8ef2cad395a27fbd3269dda5aab
Christian Hofmann <hofiorg@googlemail.com>
2018-05-03 10:14:47 +0200

    conditional wait

--- src/org/hofi/WebSocketClient.java
+++ src/org/hofi/WebSocketClient.java
@@ -5,46 +5,54 @@ import org.springframework.web.socket.WebSocketHttpHeaders;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.concurrent.ExecutionException;
-
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 class WebSocketClient {
-  private WebSocketSession webSocketSession;
+  private final Lock lock = new ReentrantLock();
+  private final Condition condition = lock.newCondition();
 
   public static void main(String[] args) throws InterruptedException, ExecutionException, URISyntaxException, IOException {
     new WebSocketClient().connect();
   }
 
-  private WebSocketClient() {
-
-  }
-
   private void connect() throws URISyntaxException, ExecutionException, InterruptedException, IOException {
-    URI endpointUrl = new URI("ws://10.2.2.14:5120/endpoint");
-    WebSocketHttpHeaders headers = new WebSocketHttpHeaders();
-    AbstractWebSocketHandler result = new AbstractWebSocketHandler() {
-      protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
-        System.out.println("Received Payload: " + message.getPayload());
-        webSocketSession.close();
-      }
-    };
+    lock.lock();
+    try {
+      URI endpointUrl = new URI("ws://10.2.2.14:5120/endpoint");
+      WebSocketHttpHeaders headers = new WebSocketHttpHeaders();
+      AbstractWebSocketHandler result = new AbstractWebSocketHandler() {
+        protected void handleTextMessage(WebSocketSession session, TextMessage message) {
+          System.out.println("Received Payload: " + message.getPayload());
+          lock.lock();
+          try {
+            condition.signal();
+          }
+          finally {
+            lock.unlock();
+          }
+        }
+      };
 
-    StandardWebSocketClient webSocketClient = new StandardWebSocketClient();
-    webSocketSession = webSocketClient
-      .doHandshake(result, headers, endpointUrl).get();
+      StandardWebSocketClient webSocketClient = new StandardWebSocketClient();
+      WebSocketSession webSocketSession = webSocketClient
+        .doHandshake(result, headers, endpointUrl).get();
 
-    TextMessage textMessage = new TextMessage("Hello World");
+      TextMessage textMessage = new TextMessage("Hello World");
 
-    webSocketSession.sendMessage(textMessage);
+      webSocketSession.sendMessage(textMessage);
 
-    while(webSocketSession.isOpen())
-    {
-      // wait
+      condition.await();
+
+      webSocketSession.close();
+    }
+    finally {
+      lock.unlock();
     }
   }
-
 }
